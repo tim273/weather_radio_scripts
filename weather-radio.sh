deac2eded685f719f06bfb3cc686d2c46b8aa70e
Tim Urberg <tim@urberg.net>
2018-04-28 12:34:21 -0500

    A couple more updates

--- weather-radio.sh
+++ weather-radio.sh
@@ -6,12 +6,11 @@
 # un-commented so only it will run.
 
 # Using LAME and EZ Stream
-#rtl_fm -f 162.55M -s 48000 -p 14 | lame --bitwidth 16 --signed -s 48000 --lowpass 3500 --abr 64 --scale 9 -r -m m - - | ezstream -c ezstream.xml
+#rtl_fm -f 162.55M -s 48000 -p 14 | lame --bitwidth 16 --signed -s 48000 --lowpass 3500 --abr 64 --scale 8 -r -m m - - | ezstream -c ezstream.xml
 
 # Using SOX and EZ Stream
-#rtl_fm -f 162.55M -s 48000 -p 14 | sox -t raw -r 48000 -b 16 -e signed -c 1 -v 5 - -r 48000 -t .mp3 -c 1 -C 64 - sinc -3.3k | ezstream -c ezstream.xml
+#rtl_fm -f 162.55M -s 48000 -p 14 | sox -t raw -r 48000 -b 16 -e signed -c 1 -v 4 - -r 48000 -t .mp3 -c 1 -C 64 - sinc -3.3k | ezstream -c ezstream.xml
 
 # Using Sox/Play with ALSA Loopback
-#rtl_fm -f 162.55M -s 48000 -p 14 | play -q -r 48000 -t raw -e s -b 16 -c 1 -V1 -v 5 -
 rtl_fm -f 162.55M -s 48000 -p 1 | play -q -r 48000 -t raw -e s -b 16 -c 1 -V1 -v 4 - sinc 100-3.1k
 
